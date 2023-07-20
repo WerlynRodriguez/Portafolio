7417337b8e2b4b61284c9d4f88bb3bc97f8a6b98
Werlyn Rodriguez <rdwerlynjose.16@gmail.com>
2023-07-20 14:09:46 -0600

    Delete src directory

--- src/components/Klotski/interfaces.tsx
+++
@@ -1,4 +0,0 @@
-export interface PieceInfo {
-    id: number
-    name: string
-}
--- src/urlData.tsx
+++
@@ -1,2 +0,0 @@
-const urlData = 'https://raw.githubusercontent.com/WerlynRodriguez/Portafolio/main/public/'
-export default urlData
